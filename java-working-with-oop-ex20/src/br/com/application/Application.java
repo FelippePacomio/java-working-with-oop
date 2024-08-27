0cc3683aa294958a97c57458514e6370f42bddde
Felippe Pacomio <felipepacomio@gmail.com>
2024-08-27 20:03:16 -0300

    Exception Handling: First Steps

---
+++ java-working-with-oop-ex20/src/br/com/application/Application.java
@@ -0,0 +1,9 @@
+package br.com.application;
+
+public class Application {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ java-working-with-oop-ex20/src/br/com/model/entities/Reservation.java
@@ -0,0 +1,65 @@
+package br.com.model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Reservation() {
+    }
+
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public long duration() {
+        long diff = checkOut.getTime() - checkIn.getTime(); //retorna a diferença dos dias em milisegundos
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public void updateDates(Date checkIn, Date checkOut) {
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n");
+        sb.append("Room ");
+        sb.append(roomNumber);
+        sb.append(", Check-in date: ");
+        sb.append(sdf.format(checkIn));
+        sb.append(", Check-Out date: ");
+        sb.append(sdf.format(checkOut));
+        sb.append(duration());
+        sb.append(" nights");
+        return sb.toString();
+    }
+
+}
