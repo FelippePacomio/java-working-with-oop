26a54c41d18e1a4c07fceeaa3cb3e1f1c080e26d
Felippe Pacomio <felipepacomio@gmail.com>
2025-01-15 21:49:08 -0300

    FileReader and BufferedReader

---
+++ java-working-with-oop-ex23/src/br/com/application/BufferReader.java
@@ -0,0 +1,36 @@
+package br.com.application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferReader {
+
+    public static void main(String[] args) {
+
+        String path = "B:\\in.txt";
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new FileReader(path));
+
+            String line = br.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
