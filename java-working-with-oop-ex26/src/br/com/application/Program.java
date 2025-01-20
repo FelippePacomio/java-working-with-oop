bb7c91ff26f8431014a920a6e1959a95c4e74055
Felippe Pacomio <felipepacomio@gmail.com>
2025-01-20 19:53:03 -0300

    Getpath, Getname and Getparent

---
+++ java-working-with-oop-ex26/src/br/com/application/Program.java
@@ -0,0 +1,19 @@
+package br.com.application;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a file path: ");
+        String strPath = sc.nextLine();
+        
+        File path = new File(strPath);
+        System.out.println("Get name: " + path.getName());
+        System.out.println("Get parent: " + path.getParent());
+        System.out.println("Get path: " + path.getPath());
+        sc.close();
+    }
+}
