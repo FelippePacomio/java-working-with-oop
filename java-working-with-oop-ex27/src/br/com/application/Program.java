5921fed3c39aad80ceab11d8b15b5b867191e9a9
Felippe Pacomio <felipepacomio@gmail.com>
2025-01-20 21:30:10 -0300

    Outputting files and folders with BufferedWriter

---
+++ java-working-with-oop-ex27/src/br/com/application/Program.java
@@ -0,0 +1,102 @@
+package br.com.application;
+
+import java.io.File;
+import java.util.Scanner;
+import br.com.entities.Item;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+    
+    static Scanner sc = new Scanner(System.in);
+    static List<Item> itemList = new ArrayList<>();
+    
+    public static void main(String[] args) {
+        menu();
+    }
+    
+    public static void menu() {
+        
+        System.out.print("\n|-------------    WELCOME      ------------|");
+        System.out.print("\n|Option 01: New item                       |");
+        System.out.print("\n|Option 02: List all items                 |");
+        System.out.print("\n|Option 03: Generate .csv file             |");
+        System.out.print("\n|Option 04: Exit                           |");
+        System.out.print("\n|------------------------------------------|");
+        
+        System.out.print("\n|Enter an option: ");
+        int selection = sc.nextInt();
+        runApplication(selection);
+    }
+    
+    public static void runApplication(int selection) {
+        try {
+            switch (selection) {
+                case 1 -> {
+                    System.out.println("Enter item name: ");
+                    String name = sc.next();
+                    sc.nextLine();
+                    System.out.println("Enter the price of " + name + " $: ");
+                    Double price = sc.nextDouble();
+                    System.out.println("Enter the quantity of " + name + ": ");
+                    Integer quantity = sc.nextInt();
+                    itemList.add(new Item(name, price, quantity));
+                    
+                    returnTo();
+                    break;
+                }
+                
+                case 2 -> {
+                    Item i = new Item();
+                    System.out.println(i.listItem(itemList));
+                    returnTo();
+                    break;
+                }
+                
+                case 3 -> {
+                    String outPath = "B:\\out\\summary.csv";
+                    
+                    boolean sucess = new File("B:\\out").mkdir();
+                    System.out.println("Directory created sucessfully: " + sucess);
+                    
+                    try (BufferedWriter bw = new BufferedWriter(new FileWriter(outPath))) {
+                        for (Item i : itemList) {
+                            bw.write(i.listItem2(itemList));
+                            bw.newLine();
+                        }
+                        System.out.println(".csv file created sucessfully!");
+                    } catch (IOException e) {
+                        System.out.println("Error: " + e.getMessage());
+                        sc.close();
+                    }
+                }
+                
+                case 4 -> {
+                    System.out.println("Thank you!");
+                    sc.close();
+                    break;
+                }
+            }
+        } catch (RuntimeException e) {
+            System.out.println("Unexpected error");
+            sc.close();
+        }
+        
+    }
+    
+    public static void returnTo() {
+        System.out.println("\n\nReturn to menu? [Y]Yes [N]No");
+        char choice = sc.next().charAt(0);
+        Character.toLowerCase(choice);
+        if (choice == 'y') {
+            menu();
+        } else {
+            System.out.println("Thank you!");
+            sc.close();
+        }
+    }
+}
---
+++ java-working-with-oop-ex27/src/br/com/entities/Item.java
@@ -0,0 +1,72 @@
+package br.com.entities;
+
+import java.util.List;
+
+public class Item {
+
+    private String name;
+    private Double price;
+    private Integer quantity;
+
+    public Item() {
+    }
+
+    public Item(String name, Double price, Integer quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String listItem(List<Item> itemList) {
+        StringBuilder sb = new StringBuilder();
+        for (Item i : itemList) {
+            sb.append("\n");
+            sb.append(i.getName());
+            sb.append(",");
+            sb.append(i.getPrice());
+            sb.append(",");
+            sb.append(i.getQuantity());
+        }
+        return sb.toString();
+    }
+
+    public String listItem2(List<Item> itemList) {
+        StringBuilder sb = new StringBuilder();
+        for (Item i : itemList) {
+            sb.append("\n");
+            sb.append(i.getName());
+            sb.append(",");
+            sb.append(calculateQuantity());
+        }
+        return sb.toString();
+    }
+
+    public Double calculateQuantity() {
+        return quantity * price;
+    }
+
+}
