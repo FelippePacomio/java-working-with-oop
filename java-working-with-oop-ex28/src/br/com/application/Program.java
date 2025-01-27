deeb5be299b9954f8121cdf0a7c29ae86fee37bb
Felippe Pacomio <felipepacomio@gmail.com>
2025-01-26 22:35:45 -0300

    Basic car rental program complete

--- java-working-with-oop-ex28/src/br/com/application/Program.java
+++ java-working-with-oop-ex28/src/br/com/application/Program.java
@@ -2,6 +2,8 @@ package br.com.application;
 
 import br.com.entities.CarRental;
 import br.com.entities.Vehicle;
+import br.com.services.BrazilTaxService;
+import br.com.services.RentalService;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
@@ -11,7 +13,6 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Locale.setDefault(Locale.US);
         menu();
 
     }
@@ -19,16 +20,33 @@ public class Program {
     public static void menu() {
 
         Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
         DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
         System.out.println("Enter rent data");
         System.out.print("Car model: ");
         String model = sc.nextLine();
         System.out.println("Rent start date: ");
-        LocalDateTime start = LocalDateTime.parse(sc.next(), fmt);
+        LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
         System.out.println("Rent finish date: ");
-        LocalDateTime finish = LocalDateTime.parse(sc.next(), fmt);
-        
+        LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+
         CarRental cr = new CarRental(start, finish, new Vehicle(model));
+
+        System.out.println("Enter the price per hour: ");
+        double pricePerHour = sc.nextDouble();
+        System.out.println("Enter the price per day: ");
+        double pricePerDay = sc.nextDouble();
+
+        RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+
+        rentalService.processInvoice(cr);
+
+        System.out.println("INVOICE: ");
+        System.out.println("Basic Payment: " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
+        System.out.println("Tax: " + String.format("%.2f", cr.getInvoice().getTax()));
+        System.out.println("Total Payment " + String.format("%.2f", cr.getInvoice().getTotalPayment()));
+
+        sc.close();
     }
 
 }
