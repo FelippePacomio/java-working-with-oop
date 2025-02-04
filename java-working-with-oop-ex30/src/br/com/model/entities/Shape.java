2cc6c73476d8e8d3cff14ec9f33b07d3535faf76
Felippe Pacomio <felipepacomio@gmail.com>
2025-02-03 21:54:36 -0300

    Abstract class, abstract methods and enums

---
+++ java-working-with-oop-ex30/src/br/com/model/entities/Shape.java
@@ -0,0 +1,23 @@
+package br.com.model.entities;
+
+import br.com.model.enums.Color;
+
+public abstract class Shape {
+
+    private Color color;
+
+    public Shape(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public abstract double area();
+
+}
---
+++ java-working-with-oop-ex30/src/br/com/model/enums/Color.java
@@ -0,0 +1,6 @@
+package br.com.model.enums;
+
+public enum Color {
+    BLACK,
+    WHITE;
+}
